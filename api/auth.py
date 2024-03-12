e5c430e3a3f14066fbf750fc0d0efee72cd31825
AllenRen <410018832@qq.com>
2024-03-13 00:20:50 +0800

    fix: 修复url路径问题

--- api/auth.py
+++ api/auth.py
@@ -17,8 +17,8 @@ def auth_headers(authorization: str) -> {bool, str}:
 
     if not api_key_or_code.startswith(access_code_prefix):
         return {"error": False, "message": "You have key", "api_key": api_key_or_code}
-
-    if api_key_or_code.replace("Bearer ", "") in authorize_code.split(","):
+    print(api_key_or_code, authorize_code.split(","), api_key_or_code in authorize_code.split(","))
+    if api_key_or_code in authorize_code.split(","):
         return {"error": False, "message": "Use System api key", "api_key": settings.OPENAI_API_KEY}
 
     return {"error": True, "message": "Invalid Authorization header provided"}
